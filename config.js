461ca22b6b3d12662e7361e5e6d7b2dd0289fb9d
ahnpro119 <ahn@komitrando.com>
2026-08-31 14:24:25 +0700

    Use published Google Sheet CSV gids

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 window.SITE_CONFIG = {
-  SHEET_ID: '1EYfHAB759ecLMpC1D6tQVsXSa6yHLIkJazQJ7gFZ-ws',
-  SHEETS: {
-    products: 'Products',
-    shipping: 'Shipping',
-    siteContent: 'SiteContent',
-    certifications: 'Certifications',
-    csr: 'CSR'
+  PUB_BASE: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR0SIjiTnZ9IBk2LwcojfJvX0wAMva2v_o93rLq5knVj2hVItMrNMg5qVk4Envg_wSM8RK66eZJhx4E/pub',
+  GIDS: {
+    products: '1163253714',
+    shipping: '2122578528',
+    siteContent: '1288972191',
+    certifications: '870934337',
+    csr: '1687412201'
   }
 };
