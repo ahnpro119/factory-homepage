a358484e365592398c3eccfd634e9b24641d2031
ahnpro119 <ahn@komitrando.com>
2026-09-07 15:08:19 +0700

    Point CMS at Homepage CMS v2 published Sheet

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 window.SITE_CONFIG = {
-  PUB_BASE: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR0SIjiTnZ9IBk2LwcojfJvX0wAMva2v_o93rLq5knVj2hVItMrNMg5qVk4Envg_wSM8RK66eZJhx4E/pub',
+  PUB_BASE: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRjZq_xftNfj4koj2Ujfvl3m_1BYn8shL1bXHbZ7JjB8jrHqDV0UqR9FeV6wKvBh84kKFM_hOmM4z2H/pub',
   GIDS: {
-    products: '1163253714',
-    shipping: '2122578528',
-    siteContent: '1288972191',
-    certifications: '870934337',
-    csr: '1687412201'
+    readme: '1630160151',
+    siteContent: '881115171',
+    factories: '1773744748',
+    process: '1182255243',
+    products: '1609975424',
+    shipping: '1887872639',
+    certifications: '432822332',
+    csr: '1937839952',
+    contact: '1705648852'
   }
 };
